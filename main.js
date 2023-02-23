996edc519e43a931b86a47ef4cf70190be6c8bc7
bbarbis <baratebarbara2020@gmail.com>
2023-02-23 15:18:15 +0100

    listak

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 window.addEventListener("load", init);
 
 function init(){
-
+    document.getElementById("tartalom").innerHTML = "<p>JavaScriptből írok ide.</p>";
+    kedvencszavak();
+}
+function kedvencszavak(){
+    const szavak = ["pizza", "csoki", "eper"]; 
+    for (let index = 0; index < szavak.length; index++) {
+     
+       
+     document.getElementById("tartalom").innerHTML += (`<p> ${szavak[index]} </p>`);
+    }
+    
 }
