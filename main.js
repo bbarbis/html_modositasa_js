f6c059cea6d57effea7cddcfa842d54184a03f78
bbarbis <baratebarbara2020@gmail.com>
2023-02-23 15:27:10 +0100

    listakfelsorolasa

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ function init(){
 }
 function kedvencszavak(){
     const szavak = ["pizza", "csoki", "eper"]; 
+    document.getElementById("tartalom").innerHTML = (`<ul>`);
     for (let index = 0; index < szavak.length; index++) {
-     
-       
-     document.getElementById("tartalom").innerHTML += (`<p> ${szavak[index]} </p>`);
+
+     document.getElementById("tartalom").innerHTML += (`<li> ${szavak[index]} </li>`);
     }
     
 }
